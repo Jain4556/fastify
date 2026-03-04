f77da1ac0597464471d03add4def733547b39a8b
Jain4556 <sunil925business@gmail.com>
2026-03-04 15:09:18 +0530

    added schema in server file and download from fastify

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const fastify = require("fastify")({logger: true})
+require('dotenv').config()
 
+const fastify = require("fastify")({ logger: true })
+const fastifyEnv = require("@fastify/env")
+fastify.register(require("@fastify/sensible"))
+fastify.register(require("@fastify/cors"))
 
-
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+fastify.register(fastifyEnv, {
+    dotenv: true,
+    schema: {
+        type: "object",
+        required: ["PORT", "MONGODB_URI", "JWT_TOKEN"],
+        properties: {
+            PORT: { type: "string", default: "3000" },
+            MONGODB_URI: { type: "string" },
+            JWT_TOKEN: { type: "string" }
+        }
+    }
 })
 
-
+fastify.get('/', async (request, reply) => {
+    return { hello: "world" }
+})
 
 const start = async () => {
     try {
-        await fastify.listen({port: 4000 })
-        fastify.log.info(
-            `Server is running at http://localhost:${process.env.PORT}`
-        )
+        await fastify.listen({ port: process.env.PORT || 3000 })
+        console.log(fastify.config)
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
